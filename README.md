ce134d6f0d1f55ed30cc26b1723bfaacf833ecb4
MuhammadBilalUmar <bilalumar001@gmail.com>
2022-11-05 15:40:50 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Candidate_Form
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9-reactive-form-validation-btopnl)
---
+++ app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from './_helpers/must-match.validator';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent implements OnInit {
+    CandidateForm: FormGroup;
+    submitted = false;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.CandidateForm = this.formBuilder.group({
+            Recruiter: ['', Validators.required],
+            Company: ['', Validators.required],
+            Candidate: ['', Validators.required],
+            Merits: ['', Validators.required],
+            Attachment: ['', Validators.required]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.CandidateForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.CandidateForm.invalid) {
+            return;
+        }
+
+        // display form values on success
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.CandidateForm.value, null, 4));
+    }
+
+    onReset() {
+        this.submitted = false;
+        this.CandidateForm.reset();
+    }
+}
